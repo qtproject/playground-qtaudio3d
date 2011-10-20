5eb42b36d2807a988b4ae458c4cc438f6f871c8f
Laszlo Papp <lpapp@kde.org>
2011-10-21 00:31:14 +0300

    Initial upload of some skeleton classes

---
+++ q_openal_export.h
@@ -0,0 +1,38 @@
+/******************************************************************************
+ * This file is part of the Mula project
+ * Copyright (c) 2011 Laszlo Papp <lpapp@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef Q_OPENAL_EXPORT_H
+#define Q_OPENAL_EXPORT_H
+
+#include <QtCore/qglobal.h>
+
+#ifndef Q_OPENAL_EXPORT
+#if defined(MAKE_Q_OPENAL_LIB)
+#define Q_OPENAL_EXPORT Q_DECL_EXPORT
+#else
+#define Q_OPENAL_EXPORT Q_DECL_IMPORT
+#endif
+#endif
+
+#ifndef Q_OPENAL_EXPORT_DEPRECATED
+#define Q_OPENAL_EXPORT_DEPRECATED Q_DECL_DEPRECATED Q_OPENAL_EXPORT
+#endif
+
+#endif // Q_OPENAL_EXPORT_H
+
---
+++ qalcontext.cpp
@@ -0,0 +1,68 @@
+/******************************************************************************
+ * This file is part of the QtOpenAL project
+ * Copyright (c) 2011 Laszlo Papp <lpapp@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "qalcontext.h"
+
+QALContext::QALContext()
+{
+}
+
+QALContext::~QALContext()
+{
+}
+
+bool
+QALContext::create(const QALContext* shareContext)
+{
+}
+
+bool
+QALContext::isValid() const
+{
+}
+
+bool
+QALContext::isSharing() const
+{
+}
+
+void
+QALContext::reset()
+{
+}
+
+bool
+QALContext::makeCurrent()
+{
+}
+
+bool
+QALContext::doneCurrent()
+{
+}
+
+ALuint
+cacheBuffer(const QString& filename)
+{
+}
+
+void*
+QALContext::getProcAddress(const QString &proc) const
+{
+}
